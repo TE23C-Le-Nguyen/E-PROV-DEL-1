1b106bfd2c508d68c122a60a904be08995a92a9e
Le gia Nguyen <le.gia@elev.ga.ntig.se>
2024-12-05 13:12:49 +0100

    first commit

---
+++ EPROVDEL1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("What is your max hp?");
+int hp = 0;
+int times = 0;
+int hpbar = 0;
+string hptext = Console.ReadLine();
+bool success = int.TryParse(hptext, out hp);
+while (false)
+{
+    Console.WriteLine("this is not a number");
+}
+
+
+while (hp > 0)
+{
+    Console.WriteLine($"[{hp}]");
+    
+    hp--;
+}
+
+List<string> name = [Console.ReadLine()];
+while (hp >= times)
+{
+    name.Add("[]");
+    times++;
+}
+Console.ReadLine();
